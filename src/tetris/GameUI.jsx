210c9a196498d2aec76d371369e68a0ae7f57986
ZhaoYuF <1445059274@qq.com>
2024-02-27 17:53:10 +0800

    游戏却UI

--- src/tetris/GameUI.jsx
+++ src/tetris/GameUI.jsx
@@ -1,8 +1,12 @@
 import React, {useMemo} from 'react'
 
 export default function GameUI() {
-    const a = useMemo()
+    const styleData = useMemo(() => {
+
+    })
     return (
-        <div style={{ position: }}>GameUI</div>
+        <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}}>
+            <div>1111</div>
+        </div>
     )
 }
