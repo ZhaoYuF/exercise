0ae0f83735235dce3152ef16c505e83e3280b8d0
ZhaoYuF <1445059274@qq.com>
2024-04-26 14:28:17 +0800

    打包

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
+
+// 插件配置
+const compressionOptions = {
+  algorithm: 'gzip',
+  ext: '.gz',
+  threshold: 10240, // 只有大于此大小的文件会被压缩（单位：字节）
+  deleteOriginFile: false, // 是否删除原文件，默认为false
+  verbose: false, // 是否显示详细日志
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression(compressionOptions),
+  ],
   server: {
     host: '0.0.0.0',
     // https: true,
